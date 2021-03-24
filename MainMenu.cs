5411a3d32bf65c245d6b1cbf2eaa8fccd9cfa743
ialfred2 <71096849+ialfred2@users.noreply.github.com>
2021-03-24 05:35:56 -0400

    Update MainMenu.cs

--- MainMenu.cs
+++ MainMenu.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
   public void PlayGame (){
-        SceneManager.LoadScene("CharacterSelection");//go to the character customizatiton scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //go to the character customizatiton scene
     }
-    public void QuitGame()//game quits
+public void QuitGame()//game quits
     {
         Debug.Log("Quit");
         UnityEditor.EditorApplication.isPlaying = false;
@@ -15,3 +15,4 @@ public class MainMenu : MonoBehaviour
     }
 
 }
+
