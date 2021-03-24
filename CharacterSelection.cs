ea1fe4e06abca36b3a127f5e2734fd9e24d07ab2
Liam Briggs <71097668+wbriggs35@users.noreply.github.com>
2021-03-24 07:19:07 -0400

    Added Comments

--- CharacterSelection.cs
+++ CharacterSelection.cs
@@ -1,61 +1,96 @@
-﻿using System.Collections;
+using System.Collections;//Libraries
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CharacterSelection : MonoBehaviour
+public class CharacterSelection : MonoBehaviour //Iterate through different character and then choose a character
 {
-    private GameObject[] characterList; //Declare game object array
-    private int index;
+    private GameObject[] _characterList; //Declare game object array
+    [SerializeField] private CharacterInfo _characterRef;//private class CharacterInfo 
+    private int _index;//Declare the index variable of the array to allow iteration through the array
+    
 
     private void Start()
     {
-        index = PlayerPrefs.GetInt("CharacterSelected");
-        characterList = new GameObject[transform.childCount];//creates an array of the amount of childern
+        _index = PlayerPrefs.GetInt("CharacterSelected");//index is initialized to the selected character in the character list
+        _characterList = new GameObject[transform.childCount];//creates an array of the amount of childern
+        
         for (int i = 0; i < transform.childCount; i++)//for each child object in the empty game object
         {
-            characterList[i] = transform.GetChild(i).gameObject;//fill the array
+            _characterList[i] = transform.GetChild(i).gameObject;//fill the array
         }
-            foreach (GameObject go in characterList)
+            foreach (GameObject go in _characterList)
             {
                 go.SetActive(false);// The characters besides the first in the array are set to false 
+                
 
-            }
-            //toggle on the selected character
-        if (characterList[index])
-        {
-            characterList[index].SetActive(true);
         }
-       
+        //toggle on the selected character
+        if (_characterList[_index])
+        {
+           
+            _characterList[_index].SetActive(true);
+
+        }
+
     }
     public void ToggleLeft()
     {
-        //Toggle off the current model
-        characterList[index].SetActive(false);
+      
 
-        index--;
-        if (index < 0)
-            index = characterList.Length - 1;
+        //Toggle off the current model
+        _characterList[_index].SetActive(false);
+
+        _index--;
+        if (_index < 0)
+        {
+            _index = _characterList.Length - 1;//loop back to the max value once the min value is passed on the left
+        }
 
         //Toggle on the new model
-        characterList[index].SetActive(true);
+        _characterList[_index].SetActive(true);
+        //translate the loaded character game object up in the y direction
+        //appears as though the character is dropping in
+        Vector3 tempPos = _characterList[_index].transform.position;
+        tempPos.y=1.2f;
+        _characterList[_index].transform.position = tempPos;
+
+
+        
     }
     public void ToggleRight()
     {
+
         //Toggle off the current model
-        characterList[index].SetActive(false);
+        _characterList[_index].SetActive(false);
 
-        index++;
-        if (index == characterList.Length)
-            index = 0;
-
+        _index++;
+        if (_index == _characterList.Length)//loop back to the min value once the max value is passed on the right
+        {
+            _index = 0;
+            
+        }
         //Toggle on the new model
-        characterList[index].SetActive(true);
+        _characterList[_index].SetActive(true);
+        //translate the loaded character game object up in the y direction
+        //appears as though the character is dropping in
+        Vector3 tempPos = _characterList[_index].transform.position;
+        tempPos.y = 1.2f;
+        _characterList[_index].transform.position = tempPos;
+
     }
 
-    public void ChangeScene()
+    public void ChangeScene() //on click of the confirm button the main scene will be initiated
     {
-        PlayerPrefs.SetInt("CharacterSelected", index);
-        SceneManager.LoadScene("Main");
+        
+        if (_index == 0)//if the character index is 0 then the character loaded will be the hero
+            _characterRef.SetHero();
+        else//if the character index is anything other than 0 then the character loaded will be the bandit
+            _characterRef.SetBandit();
+        PlayerPrefs.SetInt("CharacterSelected", _index);//sets the player that is selected
+        SceneManager.LoadScene("Main");//loads the next scene
     }
+       
+    
+        
 }
