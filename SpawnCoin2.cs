c734dcb6545c47213177728a618c73cb2da04d54
ialfred2 <71096849+ialfred2@users.noreply.github.com>
2021-03-24 05:43:04 -0400

    Add files via upload

---
+++ SpawnCoin2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnCoin2 : MonoBehaviour
+{
+
+    public GameObject Coin2;
+
+    void Start()
+    {
+        Instantiate(Coin2, new Vector3(22.219f, 0.5509f, 30.580f), Quaternion.identity);
+    }
+
+    void Update()
+    {
+
+    }
+
+}
