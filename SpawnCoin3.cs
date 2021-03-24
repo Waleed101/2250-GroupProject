7c8d8d91eb049949d391aa5fa94597618645d66e
ialfred2 <71096849+ialfred2@users.noreply.github.com>
2021-03-24 05:44:21 -0400

    Add files via upload

---
+++ SpawnCoin3.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SpawnCoin3 : MonoBehaviour
+{
+
+    public GameObject Coin3;
+
+    void Start()
+    {
+        Instantiate(Coin3, new Vector3(-33.29f, -8.324f, 72.325f), Quaternion.identity);
+    }
+
+    void Update()
+    {
+
+    }
+
+}
