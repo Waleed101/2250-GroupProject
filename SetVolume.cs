86eb4168ba2eb5458b4e809142ef18d2234493eb
Liam Briggs <71097668+wbriggs35@users.noreply.github.com>
2021-03-24 07:33:49 -0400

    Added Comments

--- SetVolume.cs
+++ SetVolume.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
+using System.Collections; //Libraries
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class SetVolume : MonoBehaviour
+public class SetVolume : MonoBehaviour //class that allows for changing the volume 
 {
     public AudioMixer mixer;
     public void SetLevel (float sliderValue)
     {
-        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20); //slider value as a log value to base ten to represent in decibels 
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20); //slider value as a log value to base ten to represent the sound on a decibel scale 
     }
 }
