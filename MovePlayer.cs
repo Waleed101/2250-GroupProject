e9eb0e80faa50e16ff4afdb254e06cadade9416b
Liam Briggs <71097668+wbriggs35@users.noreply.github.com>
2021-03-24 07:32:48 -0400

    Added Comments

--- MovePlayer.cs
+++ MovePlayer.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    private Rigidbody rb;
-    private Animator anim;
+    // Holds reference to the rigidbody and animator 
+    private Rigidbody _rb;
+    private Animator _anim;
 
+    // Mouse senstivity
     public float mouseSenstivity = 1f;
 
     public Camera cm;
 
-    private bool greeting = false;
-
-    private float timeBetweenCheck = 0.5f, timeSince = 0f;
-
-    // Double Tap Running //
-    private int _tapCount = 0, _tapsBetween = 2;
-    private float _timeToWait = 0.3f, _speedChange = 0f;
+    // Running speed
     public float speedToChange = 2f;
 
+    // Reference to the arrow prefab and where to spawn
     public GameObject arrow;
     public Transform arrowSpawn;
     public float shootForce = 20f;
@@ -27,37 +24,47 @@ public class MovePlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Lock cursor
         Cursor.visible = false;
         Screen.lockCursor = true;
-        anim = GetComponent<Animator>();
+
+        // Get animator reference
+        _anim = GetComponent<Animator>();
     }   
 
     // Update is called once per frame
     void Update()
     {
+        // Rotate player
         transform.Rotate(new Vector3(0f, Input.GetAxis("Mouse X"), 0) * mouseSenstivity);
-        anim.SetBool("Greeting", Input.GetKey(KeyCode.G));
-        anim.SetBool("Hit", Input.GetMouseButton(0));
-        anim.SetBool("Shooting", Input.GetKey(KeyCode.F));
+
+        // Turn on based on what the action currentl is
+        _anim.SetBool("Greeting", Input.GetKey(KeyCode.G));
+        _anim.SetBool("Hit", Input.GetMouseButton(0));
+        _anim.SetBool("Shooting", Input.GetKey(KeyCode.F));
+        // If they jump
         if (Input.GetKeyDown(KeyCode.Space))
-            anim.SetTrigger("Jump");
+            _anim.SetTrigger("Jump");
+
+        // Based on how they're walking/running
         if(Input.GetAxis("Vertical") > 0) {
-            anim.SetFloat("Walking", Input.GetAxis("Vertical"));
-            anim.SetFloat("Running", 0f);
+            _anim.SetFloat("Walking", Input.GetAxis("Vertical"));
+            _anim.SetFloat("Running", 0f);
             transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime);
         } else if (Input.GetAxis("Run") > 0) {
-            anim.SetFloat("Running", Input.GetAxis("Run"));
-            anim.SetFloat("Walking", 0f);
+            _anim.SetFloat("Running", Input.GetAxis("Run"));
+            _anim.SetFloat("Walking", 0f);
             transform.Translate(Vector3.forward * Input.GetAxis("Run") * Time.deltaTime * speedToChange);
         } else {
-            anim.SetFloat("Running", 0f);
-            anim.SetFloat("Walking", 0f);
+            _anim.SetFloat("Running", 0f);
+            _anim.SetFloat("Walking", 0f);
         }
     }
 
+    // Method to shoot
     public void Shoot()
     {
-        print("shoot");
+        // Arrow created and force applied
         GameObject arrowGO = Instantiate(arrow, arrowSpawn.position, arrowSpawn.rotation);
         Rigidbody arrowRB = arrowGO.GetComponent<Rigidbody>();
         arrowRB.velocity = cm.transform.forward * shootForce;
