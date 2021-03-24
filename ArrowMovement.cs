26e95ad8ecdde9433f7d50fec5553d04b339016a
Liam Briggs <71097668+wbriggs35@users.noreply.github.com>
2021-03-24 07:17:52 -0400

    Added Comments

--- ArrowMovement.cs
+++ ArrowMovement.cs
@@ -4,38 +4,46 @@ using UnityEngine;
 
 public class ArrowMovement : MonoBehaviour
 {
-    Rigidbody myBody;
-    private float lifeTimer;
-    private float timer;
-    private bool hitSomething;
+    // Field variables used to represent rigit body of the arrow as well as data of the arrow events
+    private Rigidbody _myBody;
+    private bool _hitSomething;
+
+    // Future varaibles that will be used to despawn arrows over time in phase 3
+    // private float _lifeTimer;
+    // private float _timer;
+    
     // Start is called before the first frame update
     void Start()
     {
-        myBody = GetComponent<Rigidbody>();
+        // Rigid body reference is assigned
+        _myBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!hitSomething)
+        // While the arrow is in free trajectory, the arrow will rotate naturally towards its velocity (imitating a realistic projectile)
+        if (!_hitSomething)
         {
-            transform.rotation = Quaternion.LookRotation(myBody.velocity);
+            transform.rotation = Quaternion.LookRotation(_myBody.velocity);
         }
+        // This condition is used to destroy arrows that fell through terrain that may have been caused by unwanted glitches
         if (transform.position.y < -50)
         {
             Destroy(gameObject);
         }
     }
 
+    // Once the arrow collides with another object, it calls the Stick() method to Stick() to that object
     private void OnCollisionEnter(Collision collision)
     {
-        print("hit");
-        hitSomething = true;
+        _hitSomething = true;
         Stick();
     }
 
     private void Stick()
     {
-        myBody.constraints = RigidbodyConstraints.FreezeAll;
+        // Once the arrow hits an object, it freezes its orientation so that it sticks to the object.
+        _myBody.constraints = RigidbodyConstraints.FreezeAll;
     }
 }
