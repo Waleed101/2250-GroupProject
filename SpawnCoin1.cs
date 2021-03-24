012e6a53bbfbe70e4c8911d609a26d9a45e03ccd
ialfred2 <71096849+ialfred2@users.noreply.github.com>
2021-03-24 07:52:04 -0400

    Update SpawnCoin1.cs

--- SpawnCoin1.cs
+++ SpawnCoin1.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+// Coin 1 is spawn 
 public class SpawnCoin1 : MonoBehaviour
 {
 
+   // Game Object 1
     public GameObject Coin1;
 
     void Start()
     {
+    
+        // Position of where coin 1 is spawn 
         Instantiate(Coin1, new Vector3(-64.86f, -11.875f, -44.71f), Quaternion.identity);
     }
 
