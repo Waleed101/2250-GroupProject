ce3075f6a2263b8fee496c0d909b59cf776879f1
Liam Briggs <71097668+wbriggs35@users.noreply.github.com>
2021-04-14 05:45:04 -0400

    Add comments fix bugs

--- CoinDetection.cs
+++ CoinDetection.cs
@@ -6,25 +6,22 @@ public class CoinDetection : MonoBehaviour
 
 
 {
-    public int scoreInc = 0;
+    public int scoreInc = 0; //var to increase score from objects
 
-    private void Start()
-    {
-        
-    }
+    
     public void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "Coin") {
+        if (collision.gameObject.tag == "Coin") { //if player picks up coin score increments
             // Coin sound effect
             FindObjectOfType<AudioTriggers>().PlayCoinCollect();
             FindObjectOfType<ScoreCounter>().incrementScore(scoreInc);
             Destroy(collision.gameObject);
-        } else if(collision.gameObject.tag == "EnemyCoin") {
+        } else if(collision.gameObject.tag == "EnemyCoin") { //if enemies did not steal coin their coin is worth less
             FindObjectOfType<ScoreCounter>().incrementScore(scoreInc/2);
             Destroy(collision.gameObject);
         }
 
-        if (collision.gameObject.tag == "GoldBar")
+        if (collision.gameObject.tag == "GoldBar") //same as coin with 2x value to score
         {
             FindObjectOfType<AudioTriggers>().PlayCoinCollect();
             FindObjectOfType<ScoreCounter>().incrementScore(scoreInc *2);
@@ -32,7 +29,7 @@ public class CoinDetection : MonoBehaviour
         }
 
         // References items with the tag toy and increases score by set amount of 50 points
-        if (collision.gameObject.tag == "Toy")
+        if (collision.gameObject.tag == "Toy") //same as coin with 5x value to score
         {
             FindObjectOfType<ScoreCounter>().incrementScore(scoreInc *5);
             Destroy(collision.gameObject);
